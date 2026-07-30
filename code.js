4633066acb20140037122301874f1f63a128db20
Pavlo Khomenko <pkhom@proton.me>
2026-07-31 01:38:14 +0200

    Initial files

---
+++ code.js
@@ -0,0 +1,23 @@
+function home(){
+    document.getElementById("console-text-header").innerHTML = "~/home";
+}
+
+function aboutMe(){
+    document.getElementById("console-text-header").innerHTML = "~/about_me";
+}
+
+function myMusic(){
+    document.getElementById("console-text-header").innerHTML = "~/my_music";
+}
+
+function contact(){
+    document.getElementById("console-text-header").innerHTML = "~/contact";
+}
+
+function achievements(){
+    document.getElementById("console-text-header").innerHTML = "~/achievements";
+}
+
+function politicalViews(){
+    document.getElementById("console-text-header").innerHTML = "~/political_views";
+}
