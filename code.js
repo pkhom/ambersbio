2f8d3c84c02754ddc681912d861ad5da27d2c8ff
pkhom <pkhom@proton.me>
2026-08-01 23:12:19 +0200

    added some information to display

--- code.js
+++ code.js
@@ -4,20 +4,45 @@ function home(){
 
 function aboutMe(){
     document.getElementById("console-text-header").innerHTML = "~/about_me";
+    document.getElementById("console-text-content").innerHTML = 
+    "<p id='console-text-content-element'>> Born 2008</p>";
+    document.getElementById("console-text-content").innerHTML +=
+    "<br><p id='console-text-content-element'>> IT student in the University of Ljubljana</p>";
+    document.getElementById("console-text-content").innerHTML +=
+    "<br><p id='console-text-content-element'>> Musician</p>";
+    document.getElementById("console-text-content").innerHTML +=
+    "<br><p id='console-text-content-element'>> Call me Amber</p>";
+    document.getElementById("console-text-content").innerHTML +=
+    "<br><p id='console-text-content-element'>> Pronouns: any</p>";
 }
 
 function myMusic(){
     document.getElementById("console-text-header").innerHTML = "~/my_music";
+    document.getElementById("console-text-content").innerHTML = '<p id="console-text-content-element">> </><a id="console-text-content-element" href="https://amberheart.bandcamp.com/">Bandcamp (listen for free)</a><br>';
 }
 
 function contact(){
     document.getElementById("console-text-header").innerHTML = "~/contact";
+    document.getElementById("console-text-content").innerHTML = 
+    "<p id='console-text-content-element'>> </><a id='console-text-content-element' href='https://t.me/p_khom'>Telegram</a>";
+    document.getElementById("console-text-content").innerHTML +=
+    "<br><p id='console-text-content-element'>> </><a id='console-text-content-element' href='https://discord.com/users/920455300241301655'>Discord</a>";
 }
 
 function achievements(){
     document.getElementById("console-text-header").innerHTML = "~/achievements";
+    document.getElementById("console-text-content").innerHTML =
+    "";
 }
 
 function politicalViews(){
     document.getElementById("console-text-header").innerHTML = "~/political_views";
+    document.getElementById("console-text-content").innerHTML =
+    "";
+}
+
+function projects(){
+    document.getElementById("console-text-header").innerHTML = "~/projects";
+    document.getElementById("console-text-content").innerHTML =
+    "";
 }
