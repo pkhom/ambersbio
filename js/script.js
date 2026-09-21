45d20888c17c6e10cb6c80636188970d9e4f3747
pkhom <pkhom@proton.me>
2026-09-21 16:50:10 +0200

    Added different css styles for desktop and mobile. Right now all touochscreen devices are detected as mobile, will fix later. Changed button design

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 function activateButton(button) {
-  var buttons = document.getElementsByTagName('button');
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].style = ""
-  }
+    var buttons = document.getElementsByTagName('button');
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].style = ""
+    }
 
-  button.style.backgroundColor = "#FF7500";
-  button.style.color = "#0A0505";
-  button.style.fontWeight = 600;
+    button.style.backgroundColor = "#191414";
+    button.style.border = "medium solid #FF7500";
+    button.style.fontWeight = 600;
+}
+
+
+const isMobile = !window.matchMedia('(hover: hover)').matches
+
+
+if (isMobile) {
+    document.getElementById("css").href = "css/mobile.css";
+} else {
+    document.getElementById("css").href = "css/desktop.css"
 }
